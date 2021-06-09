371c78699a21cb980c63d2d878e7cf79da16ae8d
Sofia-rib18 <sof.ribeiro213@gmail.com>
2021-06-09 11:11:46 +0100

    profile

--- Site/WayDailyOutvFuncional/js/controllers/UserController.js
+++ Site/WayDailyOutvFuncional/js/controllers/UserController.js
@@ -20,7 +20,7 @@ export default class UserController {
         } else {
             throw Error('Username/Password inválidos!');
         }
-    } 
+    }
 
     logout() {
         sessionStorage.removeItem('loggedUser')
@@ -30,17 +30,32 @@ export default class UserController {
         return sessionStorage.getItem('loggedUser') ? true : false
     }
 
-    isAdmin(){
+    //?Tem como objetivo ir buscar os dados do user para meter no editar conta
+    getUserData() {
+        let users = localStorage.getItem('users')
+        users = JSON.parse(users)
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username == this.isLogged()) {
+                return users[i]
+            }
+
+        }
+        // const user = sessionStorage.getItem('loggedUser')
+        // return this.users(this.users.filter(name => name.username === user))
+
+    }
+
+    isAdmin() {
         const name = sessionStorage.getItem('loggedUser')
-        return this.users.some(user=>user.username == name && user.type=='admin')
+        return this.users.some(user => user.username == name && user.type == 'admin')
     }
 
     delete(name) {
-        this.users (this.users.filter(username => username.name != name))
+        this.users(this.users.filter(username => username.name != name))
         localStorage.removeItem('users', JSON.stringify(this.users))
     }
-    
-    edit(username = '', password){
+
+    edit(username = '', password) {
         let user = sessionStorage['loggedUser']
         let novoPerfil = localStorage['users']
         novoPerfil = JSON.parse(novoPerfil)
@@ -49,13 +64,13 @@ export default class UserController {
             username = user
         }
 
-        for (let i = 0; i < novoPerfil.length; i++){
-            if(novoPerfil[i].username == user){
-                if (username != user){
+        for (let i = 0; i < novoPerfil.length; i++) {
+            if (novoPerfil[i].username == user) {
+                if (username != user) {
                     novoPerfil[i].username = username
                     sessionStorage.setItem('loggedUser', username)
                 }
-                if (password != ''){
+                if (password != '') {
                     novoPerfil[i].password = password
                 }
                 this.users[i] = novoPerfil[i]
@@ -63,45 +78,55 @@ export default class UserController {
             }
         }
     }
-    
 
-    userType(nome){
+
+    userType(nome) {
         let users = this.allUsers()
-        for (let i = 0; i < users.length; i++){
-            if(users[i].username == nome){
-               let tipo = users[i].type
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username == nome) {
+                let tipo = users[i].type
 
-                if (tipo == 'admin'){
+                if (tipo == 'admin') {
                     tipo = 'user'
                 }
-                else if (tipo == 'user'){
+                else if (tipo == 'user') {
                     tipo = 'admin'
                 }
 
                 users[i].type = tipo
                 this.users[i] = users[i]
-                localStorage.setItem('users', JSON.stringify(this.users)) 
+                localStorage.setItem('users', JSON.stringify(this.users))
             }
-            
+
         }
     }
 
-    stades(nome){
+    stades(nome) {
         let user = this.allUsers()
-        for (let i = 0; i < user.length; i++){
-            if (user[i].username == nome){
+        for (let i = 0; i < user.length; i++) {
+            if (user[i].username == nome) {
                 const stade = user[i].stade
-                
+
                 return stade
             }
         }
     }
 
-        allUsers(){
-            let users = localStorage['users']
-            users = JSON.parse(users)
-            return users
+    allUsers() {
+        let users = localStorage['users']
+        users = JSON.parse(users)
+        return users
+    }
+
+
+    //Mudar os dados do utilizador
+    change(name, gender, email, date, local, password) {
+        if (this.users.some(user => user.username === name)) {
+            this.users.pop(UserModel(name, gender, email, date, local, password))
+            this.users.push(new UserModel(name, gender, email, date, local, password));
+            localStorage.setItem('users', JSON.stringify(this.users))
         }
+    }
 
 }
 
